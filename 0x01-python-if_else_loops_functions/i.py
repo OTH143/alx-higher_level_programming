84590240d91acdee2af8531ed102fd9752ec0b78
OTH143 <toyintaiwohammed@gmail.com>
2023-01-04 12:37:24 +0000

    yeah!!

---
+++ 0x01-python-if_else_loops_functions/i.py
@@ -0,0 +1,3 @@
+num = 5
+mun = 4
+print("My first number is", num "and my second number is", {mun}),
---
+++ 0x01-python-if_else_loops_functions/tpye.py
@@ -0,0 +1,3 @@
+number = 799
+last_dig = 34
+print("The difference is in {} and {}", number, last_dig)
